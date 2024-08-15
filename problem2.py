08022644fb281642d1d8c4e6b487f4d605e771c1
Schnretzl <wrhendley@gmail.com>
2024-08-15 14:39:34 -0500

    Add problem 2

---
+++ problem2.py
@@ -0,0 +1,21 @@
+# 2. The Greatest Showdown
+
+# Task 1: Identify the Greatest
+first = int(input('Please enter the first number: '))
+second = int(input('Please enter the second number: '))
+third = int(input('Please enter the third number: '))
+
+if first >= second and first >= third:
+    print(f'{first} is the largest number.')
+elif second >= first and second >= third:
+    print(f'{second} is the largest number.')
+else:
+    print(f'{third} is the largest number.')
+    
+# Task 2: Identify the Smallest
+if first <= second and first <= third:
+    print(f'{first} is the smallest number.')
+elif second <= first and second <= third:
+    print(f'{second} is the smallest number.')
+elif third <= second and third <= third:
+    print(f'{third} is the smallest number.')
